15e26320133517d6f90d1adafdd7221d15107c66
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2021-12-09 15:31:00 -0300

    Deploy

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./logo192.png" className="App-logo" alt="logo" />
+        <p>
+          Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprenda ReactJS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
